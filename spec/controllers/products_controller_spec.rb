59e634b8a5dcb315b1584a7d336a1465da0654a4
Eric Tillberg <eric.tillberg@gmail.com>
2016-05-02 15:38:51 -0400

    added a couple products controller tests

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'database_cleaner'
+
+RSpec.describe ProductsController, type: :controller do
+  setup :activate_authlogic
+  before(:each) do
+    request.env["HTTP_REFERER"] = "http://localhost:3000/"
+  end
+
+  describe "POST #add_to_cart" do
+    before :each do
+      user = FactoryGirl.create :user
+      log_in(user)
+      product = FactoryGirl.create :product
+      post :add_to_cart, product_id: product
+    end
+    context "user has no shopping cart yet" do
+      it "creates a new shopping cart" do
+        expect(ShoppingCart.count).to eq(1)
+      end
+    end
+
+    context "user has a shopping cart" do
+      it "adds a product to the cart" do
+        expect(ShoppingCart.first.shopping_cart_items.first.quantity).to eq(1)
+      end
+    end
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+require 'ffaker'
+
+FactoryGirl.define do
+  factory :user do
+    email { FFaker::Internet.email }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
